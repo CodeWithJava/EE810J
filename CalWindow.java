bb7e735ebfca1c610e6ef32e7e3d5bb1d39a60a4
VCneverdie <vivienne.cuiy@gmail.com>
2016-04-18 22:01:00 -0400

    Just a frame

---
+++ CalWindow.java
@@ -0,0 +1,20 @@
+//package Algebra;
+/**
+CopyRight:
+Project:Algebra Solver
+Comment:Teamwork
+Course: EE 810 J Engineering Programming in Java
+Title: AlgebraSolver Main Method
+JDK Version: 1.8.0_77
+Author: (Alphabeta Order) Songnian Yin, Yabin Han, Ying Cui
+Create Date: March 26th 2016
+Finish Date: Until Now
+Description: A program which can solve algebra
+*/
+
+import java.awt.*;
+import javax.swing.*;
+
+public class CalWindow extends JFrame{
+
+}
